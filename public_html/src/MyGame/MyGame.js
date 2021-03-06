842b246ed6c023c7230d4839f2777737c14a4ee7
Aldamaer <72674861+Aldamaer@users.noreply.github.com>
2021-03-06 12:37:00 -0800

    updated myGame for smooth terrain example

--- public_html/src/MyGame/MyGame.js
+++ public_html/src/MyGame/MyGame.js
@@ -1,6 +1,6 @@
 /*
- * File: MyGame.js 
- * This is the logic of our game. 
+ * File: MyGame.js
+ * This is the logic of our game.
  */
 
 /*jslint node: true, vars: true */
@@ -17,26 +17,37 @@ function MyGame() {
 
     this.mMsg = null;
 
-    this.mLineSet = [];
-    this.mCurrentLine = null;
-    this.mP1 = null;
-    
     this.testDirt = null;
-    
     this.dirtTexture = "assets/MC_Dirt.png";
+    this.stoneTexture = "assets/MC_Stone.png";
+    this.diamondTexture = "assets/MC_Diamond.png";
+    this.pinkBrickTexture = "assets/Terraria_PinkBrick.png";
     this.dirtTile = null;
+    this.arr = null;
+    this.nextPos = null;
+    this.tileSize = null;
+
+    this.mWorldMatrix = null;
+    this.mTiers = null;
+
 }
 gEngine.Core.inheritPrototype(MyGame, Scene);
 
 MyGame.prototype.loadScene = function () {
     // loads the textures
     gEngine.Textures.loadTexture(this.dirtTexture);
+    gEngine.Textures.loadTexture(this.stoneTexture);
+    gEngine.Textures.loadTexture(this.diamondTexture);
+    gEngine.Textures.loadTexture(this.pinkBrickTexture);
 };
 
 MyGame.prototype.unloadScene = function () {
     // Game loop not running, unload all assets
 
     gEngine.Textures.unloadTexture(this.dirtTexture);
+    gEngine.Textures.unloadTexture(this.stoneTexture);
+    gEngine.Textures.unloadTexture(this.diamondTexture);
+    gEngine.Textures.unloadTexture(this.pinkBrickTexture);
 };
 
 MyGame.prototype.initialize = function () {
@@ -49,18 +60,23 @@ MyGame.prototype.initialize = function () {
     this.mCamera.setBackgroundColor([0.8, 0.8, 0.8, 1]);
             // sets the background to gray
 
+    this.arr = [];
+    this.nextPos = [-17.5, -7.5];
+    this.tileSize = [2, 2];
+
+    this.mWorldMatrix = null;
+    this.mTiers = [ [0, 1],
+                    [1, 3],
+                    [3, 4],
+                    [3, 5],
+                    [7, 10] ];
+
     this.mMsg = new FontRenderable("Status Message");
     this.mMsg.setColor([0, 0, 0, 1]);
     this.mMsg.getXform().setPosition(-19, -8);
     this.mMsg.setTextHeight(3);
-    
-    // this.testDirt = new TextureRenderable(this.dirtTexture);
-    // this.testDirt.setColor([0, 0, 0, 0]);
-    // this.testDirt.getXform().setPosition(5, 5);
-    // this.testDirt.getXform().setSize(5, 5);
-    
-    this.dirtTile = new Tile(this.dirtTexture, [5, 5], [5, 5]);
-    this.dirtTile.setRarity(50);
+
+    // this.dirtTile.setRarity(50);
 };
 
 // This is the draw function, make sure to setup proper drawing environment, and more
@@ -70,23 +86,33 @@ MyGame.prototype.draw = function () {
     gEngine.Core.clearCanvas([0.9, 0.9, 0.9, 1.0]); // clear to light gray
 
     this.mCamera.setupViewProjection();
-    var i, l;
-    for (i = 0; i < this.mLineSet.length; i++) {
-        l = this.mLineSet[i];
-        l.draw(this.mCamera);
-    }
-    this.mMsg.draw(this.mCamera);   // only draw status in the main camera
-    
-    this.dirtTile.draw(this.mCamera);
+    if (this.mWorldMatrix !== null)
+      this.mWorldMatrix.draw(this.mCamera);
 };
 
 // The Update function, updates the application state. Make sure to _NOT_ draw
 // anything from this function!
 MyGame.prototype.update = function () {
-    var msg = "Lines: " + this.mLineSet.length + " ";
-    var echo = "";
-    var x, y;
+  if (gEngine.Input.isKeyClicked(gEngine.Input.keys.Space))
+  {
+    var arrOfLayers = [this.pinkBrickTexture,
+                       this.diamondTexture,
+                       this.stoneTexture,
+                       this.dirtTexture];
+    var layerTiers = [3, 0, 4, 3];
 
-    msg += echo;
-    this.mMsg.setText(msg);
+    this.mWorldMatrix = new WorldMatrix(arrOfLayers,                   // array of the layers
+                                        layerTiers,                    // array of the tiers for the layers
+                                        this.mTiers,                   // array of standard tier values
+                                        [-20 + (this.tileSize[0]/2),   // starting position
+                                         -10 + (this.tileSize[1]/2)],
+                                        this.tileSize);                // tile size
+
+    this.mWorldMatrix.generateWorld(100/this.tileSize[0]);
+  }
+
+  if (gEngine.Input.isKeyClicked(gEngine.Input.keys.S))
+  {
+    this.mWorldMatrix.smoothTerrain();
+  }
 };
